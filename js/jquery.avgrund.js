9f4dae3b6fdf809689db805bb1f4c61a7171ab2b
Polarblau <polarblau@gmail.com>
2012-09-03 10:23:11 +0300

    Replace tabs with spaces, fix closing.

--- js/jquery.avgrund.js
+++ js/jquery.avgrund.js
@@ -6,90 +6,91 @@
  */
 
 (function($) {
-	$.fn.avgrund = function(options) {
-		var defaults = {
-			width               : 380,
-			height              : 280,
-			showClose           : false,
-			showCloseText       : '',
-			closeByEscape       : true,
-			closeByDocument     : true,
-			holderClass         : '',
-			overlayClass        : '',
-			enableStackAnimation: false,
-			onBlurContainer     : ''
-		};
-		var options = $.extend(defaults, options);
+  $.fn.avgrund = function(options) {
+    var defaults = {
+      width               : 380,
+      height              : 280,
+      showClose           : false,
+      showCloseText       : '',
+      closeByEscape       : true,
+      closeByDocument     : true,
+      holderClass         : '',
+      overlayClass        : '',
+      enableStackAnimation: false,
+      onBlurContainer     : ''
+    };
+    var options = $.extend(defaults, options);
 
-		return this.each(function() {
-		  var $this   = $(this),
-			    $body   = $('body');
+    return this.each(function() {
+      var $this   = $(this),
+          $body   = $('body');
       
-      // this is the first time we call this plugin
+      // has this plugin be called before? if yes, re-use some stuff
       if ($body.hasClass('avgrund-ready')) {
-        $dialog = $('body > avgrund-popin');
+        $dialog = $('body > .avgrund-popin');
+        
       } else {
         $dialog = $('<div class="avgrund-popin ' + options.holderClass + '"></div>');
         $body
-  			  .addClass('avgrund-ready')
-  			  .append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>')
-  			  .append($dialog);
+          .addClass('avgrund-ready')
+          .append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>')
+          .append($dialog);
       }
-			
-			$('.avgrund-popin').css({
-				'width'     : options.width + 'px',
-				'height'    : options.height + 'px',
-				'marginLeft': '-' + (options.width / 2 + 10) + 'px',
-				'marginTop' : '-' + (options.height / 2 + 10) + 'px'
-			});
+      
+      $('.avgrund-popin').css({
+        'width'     : options.width + 'px',
+        'height'    : options.height + 'px',
+        'marginLeft': '-' + (options.width / 2 + 10) + 'px',
+        'marginTop' : '-' + (options.height / 2 + 10) + 'px'
+      });
 
-			if (options.showClose === true) {
-				$('.avgrund-popin').append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
-			}
+      if (options.showClose === true) {
+        $dialog.append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
+      }
 
-			if (options.enableStackAnimation === true) {
-				$('.avgrund-popin').addClass('stack');
-			}
+      if (options.enableStackAnimation === true) {
+        $dialog.addClass('stack');
+      }
 
-			if (options.onBlurContainer != '') {
-				$(options.onBlurContainer).addClass('avgrund-blur');
-			}
-			
-			// close popup by clicking Esc button
-			function onDocumentKeyup(e) {
-				if (options.closeByEscape === true && e.keyCode === 27) {
-					hide();
-				}
-			}
-			
-			// close popup by clicking outside it
-			function onDocumentClick(e) {
-			  if (options.closeByDocument === true && $(e.target).is('.avgrund-overlay') ||
-			      $(e.target).is('.avgrund-close')) {
-			      hide();
-			  }      
-			}
+      if (options.onBlurContainer != '') {
+        $(options.onBlurContainer).addClass('avgrund-blur');
+      }
+      
+      // close popup by clicking Esc button
+      function onDocumentKeyup(e) {
+        if (options.closeByEscape === true && e.keyCode === 27) {
+          hide();
+        }
+      }
+      
+      // close popup by clicking outside it
+      function onDocumentClick(e) {
+        if (options.closeByDocument === true && $(e.target).is('.avgrund-overlay') ||
+            $(e.target).is('.avgrund-close')) {
+            hide();
+        }      
+      }
 
-			// show popup
-			function show(content) {
-			  $dialog.html(content);
-				$body
-				  .on('keyup', onDocumentKeyup)
-				  .on('click', onDocumentClick)
+      // show popup
+      function show(content) {
+        $dialog.append(content);
+        $body
+          .on('keyup', onDocumentKeyup)
+          .on('click', onDocumentClick)
           .addClass('avgrund-active');
-			}
+      }
 
-			// hide popup
-			function hide() {
-				$body
-				  .off('keyup', onDocumentKeyup)
-				  .off('click', onDocumentClick)
+      // hide popup
+      function hide() {
+        $body
+          .off('keyup', onDocumentKeyup)
+          .off('click', onDocumentClick)
           .removeClass('avgrund-active');
-			}
+      }
       
       show($this);
         
-		});
+    });
 
-	}
+  }
 })(jQuery)
